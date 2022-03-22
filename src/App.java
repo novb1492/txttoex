722f7b5798fba1a7cd2eaa797169a4e0a83fad6f
kimjunyoung <kimjy80@sesisoft.com>
2022-03-22 14:52:33 +0900

    성공했는데 쓰레기값 왜들어감?

--- src/App.java
+++ src/App.java
@@ -1,10 +1,17 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.swing.GroupLayout.Alignment;
+
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -20,6 +27,7 @@ public class App {
             String fileName=f.getName();
             System.out.println(fileName);
             BufferedReader txtFile = new BufferedReader(new FileReader(path+"/"+fileName));  
+            int rowNum=0;
             while (true) {
                 String text=txtFile.readLine();
                 if(text==null){
@@ -35,12 +43,31 @@ public class App {
             if(flag){
                 XSSFWorkbook xssfWb =  new XSSFWorkbook(); 
                 XSSFSheet xssfSheet = xssfWb.createSheet(fileName);
+                XSSFRow xssfRow = xssfSheet.createRow(0);
+                XSSFCell xssfCell = xssfRow.createCell((short) 0);
+                xssfCell.setCellValue("jpan");
+                xssfCell = xssfRow.createCell((short) 1);
+                xssfCell.setCellValue("kr");
                 for(String text3:texts){
-                    
+                    rowNum=rowNum+=1;
+                    xssfRow = xssfSheet.createRow(rowNum);
+                    xssfCell = xssfRow.createCell((short) 0);
+                    xssfCell.setCellValue(text3);
+                    //xssfCell= xssfRow.createCell((short) 1);
+                    //xssfCell.setCellValue("text3");
                 }
-                flag=false;
-            }
+                String localFile = path + "/"+fileName+"ex.xlsx";
+                File file = new File(localFile);
+                FileOutputStream fos = null;
+                fos = new FileOutputStream(file);
+                xssfWb.write(fos);
+
+                if (xssfWb != null)	xssfWb.close();
+                if (fos != null) fos.close();
+                    flag=false;
+                }
         }
+
        } catch (Exception e) {
            e.printStackTrace();
        }
